19415c5668156fb179a64d97e942fea8ee747e1b
Andy Gayton <andy@thecablelounge.com>
2021-10-30 16:36:57 -0400

    pull logic to navigate stream into a struct

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,115 @@ use std::fs;
 use std::io;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::io::Seek;
 use std::io::Write;
+use std::iter::Iterator;
 use std::os::unix::io::AsRawFd;
 
 use clap::{App, Arg};
 
 use scopeguard;
 
-use termion::event::Key;
-use termion::input::TermRead;
+// use termion::event::Key;
+// use termion::input::TermRead;
 
 use termios;
+
+struct Stream<R> {
+    inner: R,
+    store: Vec<String>,
+    cursor: usize,
+}
+
+impl<R: Iterator<Item = String>> Stream<R> {
+    // Returns None if the provided iter has no values
+    pub fn from(mut inner: R) -> Option<Stream<R>> {
+        // this will block until an initial item has been read
+        let s = inner.next()?;
+
+        let store: Vec<String> = vec![s];
+
+        Some(Stream {
+            inner: inner,
+            store: store,
+            cursor: 0,
+        })
+    }
+
+    // None means the whole input stream has been consumed
+    pub fn current(&self) -> Option<&str> {
+        Some(&self.store[self.cursor][..])
+    }
+
+    // None means we are at the boundary, so the cursor cannot be moved
+    pub fn move_right(&mut self) -> Option<&str> {
+        if self.cursor < self.store.len() - 1 {
+            self.cursor += 1;
+            return self.current();
+        }
+
+        let next = self.inner.next()?;
+        self.store.push(next);
+        self.cursor += 1;
+        self.current()
+    }
+
+    // None means we are at the boundary, so the cursor cannot be moved
+    pub fn move_left(&mut self) -> Option<&str> {
+        if self.cursor == 0 {
+            return None;
+        }
+        self.cursor -= 1;
+        self.current()
+    }
+
+    // None means the whole input stream has been consumed
+    pub fn remove(&mut self) -> Option<&str> {
+        self.store.remove(self.cursor);
+        if self.store.len() == 0 {
+            let next = self.inner.next()?;
+            self.store.push(next);
+        }
+        self.current()
+    }
+}
+
+#[test]
+fn test_stream_navigation() {
+    let mut stdin = io::Cursor::new(Vec::new());
+    writeln!(stdin, "one\ntwo\nthree").unwrap();
+    stdin.seek(io::SeekFrom::Start(0)).unwrap();
+    let stdin = BufReader::new(stdin).lines().map(|x| x.unwrap());
+
+    let mut stream = Stream::from(stdin).expect("stdin has values");
+
+    assert_eq!(stream.current(), Some("one"));
+    assert_eq!(stream.move_right(), Some("two"));
+    assert_eq!(stream.move_right(), Some("three"));
+    assert_eq!(stream.move_right(), None);
+    assert_eq!(stream.move_left(), Some("two"));
+    assert_eq!(stream.move_left(), Some("one"));
+    assert_eq!(stream.move_left(), None);
+    assert_eq!(stream.move_right(), Some("two"));
+    assert_eq!(stream.move_right(), Some("three"));
+    assert_eq!(stream.move_right(), None);
+}
+
+#[test]
+fn test_stream_remove() {
+    let mut stdin = io::Cursor::new(Vec::new());
+    writeln!(stdin, "one\ntwo\nthree").unwrap();
+    stdin.seek(io::SeekFrom::Start(0)).unwrap();
+    let stdin = BufReader::new(stdin).lines().map(|x| x.unwrap());
+
+    let mut stream = Stream::from(stdin).expect("stdin has values");
+
+    assert_eq!(stream.remove(), Some("two"));
+    assert_eq!(stream.move_right(), Some("three"));
+    assert_eq!(stream.move_left(), Some("two"));
+    assert_eq!(stream.remove(), Some("three"));
+    assert_eq!(stream.remove(), None);
+}
 
 fn main() -> io::Result<()> {
     let matches = App::new("x-select")
@@ -49,7 +147,7 @@ fn main() -> io::Result<()> {
     ios.c_lflag &= !(termios::ECHO | termios::ICANON);
     termios::tcsetattr(f.as_raw_fd(), termios::TCSANOW, &ios)?;
 
-    let mut guarded = scopeguard::guard(f, |f| {
+    let _guarded = scopeguard::guard(f, |f| {
         termios::tcsetattr(f.as_raw_fd(), termios::TCSANOW, &prev_ios).unwrap();
     });
 
@@ -59,7 +157,7 @@ fn main() -> io::Result<()> {
     let mut stdout = io::stdout();
 
     let mut options = Vec::new();
-    let mut cursor = 0;
+    let cursor = 0;
 
     if let Some(line) = stdin.next() {
         options.push(line);
@@ -68,6 +166,7 @@ fn main() -> io::Result<()> {
         return Ok(());
     }
 
+    /*
     for key in guarded.by_ref().keys() {
         let key = key.unwrap();
 
@@ -114,6 +213,7 @@ fn main() -> io::Result<()> {
             }
         }
     }
+    */
 
     Ok(())
 }
