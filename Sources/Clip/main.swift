228bfc0973ba82e9d18945644a7074cf1f6d7132
Andy Gayton <andy@thecablelounge.com>
2021-11-09 09:07:17 -0500

    write a JSON payload to STDOUT when new items are copied to the pasteboard

--- Sources/Clip/main.swift
+++ Sources/Clip/main.swift
@@ -1,21 +1,34 @@
 import AppKit
 
 let pasteboard = NSPasteboard.general
-var changeCount = 0
+var changeCount = pasteboard.changeCount
 
 while true {
     let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+        if changeCount != pasteboard.changeCount {
+            let sourceApp: NSRunningApplication? = NSWorkspace.shared.frontmostApplication
+            let source = sourceApp?.bundleIdentifier ?? "unknown"
 
-    if changeCount != pasteboard.changeCount {
-        let sourceApp: NSRunningApplication? = NSWorkspace.shared.frontmostApplication
-        pasteboard.pasteboardItems?.forEach({ item in
-            item.types.forEach({ x in
-            let d = pasteboard.data(forType: x)
-            print(sourceApp?.bundleIdentifier as Any, x, d!.count)
-            })
-        })
-    }
-    changeCount = pasteboard.changeCount
+            var row = [
+                "change": pasteboard.changeCount,
+                "source": source as Any,
+            ]
+            var types = [String: String]()
+
+            pasteboard.pasteboardItems?.forEach { item in
+                item.types.forEach { x in
+                    if let data = pasteboard.data(forType: x) {
+                        types[x.rawValue] = data.base64EncodedString()
+                    }
+                }
+            }
+
+            row["types"] = types
+            var data = try! JSONSerialization.data(withJSONObject: row)
+            data.append("\n".data(using: .utf8)!)
+            FileHandle.standardOutput.write(data)
+        }
+        changeCount = pasteboard.changeCount
     }
 
     let runLoop = RunLoop.main
