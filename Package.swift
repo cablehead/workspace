05ee10e2799e632f4dd4887eb9953d09f7bf1acc
Andy Gayton <andy@thecablelounge.com>
2021-11-09 09:07:30 -0500

    swiftformat

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Clip",
     platforms: [
-        .macOS(.v10_12) // v10.12 is needed for scheduledTimer usage
+        .macOS(.v10_12), // v10.12 is needed for scheduledTimer usage
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,9 +17,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Clip",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "ClipTests",
-            dependencies: ["Clip"]),
+            dependencies: ["Clip"]
+        ),
     ]
 )
