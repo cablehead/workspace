2d9ad08c791346421c47d6c3903d6ffa907cd955
Andy Gayton <andy@thecablelounge.com>
2022-07-27 21:46:19 -0400

    capture the current time in milliseconds

--- xs/src/main.rs
+++ xs/src/main.rs
@@ -1,6 +1,10 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use clap::{Parser, Subcommand};
 
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection, params};
+
+use anyhow::Result;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -28,6 +32,7 @@ enum Commands {
 struct Item {
     id: i32,
     topic: String,
+    stamp: u128,
 }
 
 fn main() -> Result<()> {
@@ -53,7 +58,6 @@ fn create(conn: &Connection) -> Result<()> {
         /*
         "CREATE TABLE IF NOT EXISTS stream (
            id INTEGER PRIMARY KEY,
-           stamp INTEGER NOT NULL,
            source_id INTEGER,
            parent_id INTEGER,
            topic TEXT NOT NULL,
@@ -64,7 +68,8 @@ fn create(conn: &Connection) -> Result<()> {
         */
         "CREATE TABLE IF NOT EXISTS stream (
            id INTEGER PRIMARY KEY,
-           topic TEXT NOT NULL
+           topic TEXT NOT NULL,
+           stamp BLOB NOT NULL
         )",
         [],
     )?;
@@ -73,23 +78,22 @@ fn create(conn: &Connection) -> Result<()> {
 }
 
 fn insert(conn: &Connection, name: &String) -> Result<()> {
+    let stamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_millis();
     conn.execute(
-        "INSERT INTO stream (topic) values (?1)",
-        &[&name.to_string()],
+        "INSERT INTO stream (topic, stamp) values (?1, ?2)",
+        params![&name.to_string(), stamp.to_le_bytes()],
     )?;
-
     Ok(())
 }
 
 fn list(conn: &Connection) -> Result<()> {
-    let mut stmt = conn.prepare(
-        "select * from stream;"
-    )?;
+    let mut stmt = conn.prepare("select * from stream;")?;
 
     let items = stmt.query_map([], |row| {
         Ok(Item {
             id: row.get(0)?,
             topic: row.get(1)?,
+            stamp: u128::from_le_bytes(row.get(2)?),
         })
     })?;
 
