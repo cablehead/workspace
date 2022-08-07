2538334381064d1054f82a7e7a2e5865de545ded
Andy Gayton <andy@thecablelounge.com>
2022-08-07 15:30:18 -0400

    listen for both http and websockets

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-#![deny(warnings)]
+use futures_util::{FutureExt, StreamExt};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let index = warp::method()
+    let ws = warp::ws().map(|ws: warp::ws::Ws| {
+        // And then our closure will be called when it completes...
+        ws.on_upgrade(|websocket| {
+            // Just echo all messages back...
+            let (tx, rx) = websocket.split();
+            rx.forward(tx).map(|result| {
+                if let Err(e) = result {
+                    eprintln!("websocket error: {:?}", e);
+                }
+            })
+        })
+    });
+
+    let http = warp::method()
         .and(warp::path::full())
         .and(warp::header::headers_cloned())
-        .map(|method, path, headers| {
+        .and(warp::body::bytes())
+        .map(|method, path, headers, body| {
             println!("{:?}", method);
             println!("{:?}", path);
             println!("{:?}", headers);
+            println!("{:?}", body);
             "hello"
         });
-    warp::serve(index).run(([127, 0, 0, 1], 3030)).await;
+
+    warp::serve(ws.or(http)).run(([127, 0, 0, 1], 3030)).await;
 }
