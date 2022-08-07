cfdb21ef3af6f0ade6871db66eb4f90653e6c470
Andy Gayton <andy@thecablelounge.com>
2022-08-07 14:33:30 -0400

    accept all

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let index = warp::path::end()
-        .and(warp::get())
+    let index = warp::method()
+        .and(warp::path::full())
         .and(warp::header::headers_cloned())
-        .map(|h| {
-            println!("{:?}", h);
+        .map(|method, path, headers| {
+            println!("{:?}", method);
+            println!("{:?}", path);
+            println!("{:?}", headers);
             "hello"
         });
     warp::serve(index).run(([127, 0, 0, 1], 3030)).await;
