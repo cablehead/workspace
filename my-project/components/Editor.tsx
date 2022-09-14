8b894ea622a27a166d4edbc74bbf0c899e23369d
Andy Gayton <andy@thecablelounge.com>
2022-09-14 10:38:00 -0400

    add an event handler for ctrl-r

--- my-project/components/Editor.tsx
+++ my-project/components/Editor.tsx
@@ -9,7 +9,28 @@ export function Editor(props) {
     textarea.current.select();
   }, []);
 
+  const handler = (event) => {
+    console.log("Editor", event);
+    switch (true) {
+      case event.ctrlKey && event.key == "r":
+        console.log("run", textarea.current.value);
+        event.preventDefault();
+        break;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handler);
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
   return (
-    <textarea ref={textarea} style="height:100%; width:100%; resize: none;" {...props} />
+    <textarea
+      ref={textarea}
+      style="height:100%; width:100%; resize: none;"
+      {...props}
+    />
   );
 }
