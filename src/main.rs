7afdfbf097a6cb8880b10aa58cf67c52daa8a265
Andy Gayton <andy@thecablelounge.com>
2022-11-21 15:13:10 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
+use lmdb::Cursor;
 use lmdb::Transaction;
 
 fn main() {
     let path = std::path::Path::new("./foo");
-    std::fs::create_dir_all(path).unwrap();
+    // std::fs::create_dir_all(path).unwrap();
+    let command: &str = &std::env::args().nth(1).unwrap();
+
     let env = lmdb::Environment::new().open(path).unwrap();
     let db = env.open_db(None).unwrap();
-    /*
-    {
-        let mut txn = env.begin_rw_txn().unwrap();
-        txn.put(db, &"foo", &"bar", lmdb::WriteFlags::empty())
-            .unwrap();
-        txn.commit().unwrap();
-    }
-    */
 
-    let txn = env.begin_ro_txn().unwrap();
-    let value = txn.get(db, &"foo").unwrap();
-    let value = std::str::from_utf8(value).unwrap();
-    println!("{}", value);
+    match command {
+        "put" => {
+            let key = scru128::new();
+
+            let mut txn = env.begin_rw_txn().unwrap();
+            txn.put(
+                db,
+                &key.to_u128().to_ne_bytes(),
+                &"bar",
+                lmdb::WriteFlags::empty(),
+            )
+            .unwrap();
+            txn.commit().unwrap();
+        }
+
+        "cat" => {
+            let txn = env.begin_ro_txn().unwrap();
+            let c = txn.open_ro_cursor(db).unwrap();
+            let (key, value) = c.get(None, None, lmdb_sys::MDB_LAST).unwrap();
+            let key = scru128::Scru128Id::from_u128(u128::from_ne_bytes(
+                key.unwrap().try_into().unwrap(),
+            ));
+            let value = std::str::from_utf8(value).unwrap();
+            println!("{:?} {:?} {:?}", key.timestamp(), value, lmdb_sys::MDB_LAST);
+        }
+
+        _ => panic!(),
+    }
 }
